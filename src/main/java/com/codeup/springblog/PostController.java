66e92ab3f00ed71394f56246376b6c7ee1569282
MichaelWaymire <mike.waymire2@gmail.com>
2018-10-29 11:18:16 -0500

    created PostController

---
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -0,0 +1,39 @@
+package com.codeup.springblog;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String postPage() {
+        return "Post Index Page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postId(@PathVariable long id) {
+        return "View an individual post" + id;
+    }
+
+    @GetMapping("/posts/cr/create")
+    @ResponseBody
+    public String createPost() {
+        return "Create a new post";
+    }
+
+    @PostMapping("/posts/create")
+    public @ResponseBody
+    ResponseEntity<String> post() {
+        return new ResponseEntity<String>("POST Response", HttpStatus.OK);
+    }
+
+
+}
