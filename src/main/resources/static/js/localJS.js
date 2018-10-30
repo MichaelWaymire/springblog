f3fe341b3967dfadad778810457fbb530b1b7fb1
MichaelWaymire <mike.waymire2@gmail.com>
2018-10-30 10:37:25 -0500

    workin on exercise 1 for views

---
+++ src/main/resources/static/js/localJS.js
@@ -0,0 +1,19 @@
+function diceRoll() {
+	var rollTheDice = Math.floor((Math.random() * 6) + 1);
+
+	if(rollTheDice == 1){
+		return "1";
+	} else if ( rollTheDice == 2){
+		return "2";
+	}else if (rollTheDice == 3){
+		return "3";
+	}else if (rollTheDice == 4){
+		return "4";
+	}else if (rollTheDice == 5){
+		return "5";
+	}else{
+		rollTheDice == 6
+		return "7";
+	}
+
+}
