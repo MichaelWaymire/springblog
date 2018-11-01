8c050446f9df9e2c84c7bd6fb9754f93d576777d
MichaelWaymire <mike.waymire2@gmail.com>
2018-11-01 14:33:26 -0500

    Created PostRepo Interface

--- src/main/java/com/codeup/springblog/controllers/Post.java
+++ src/main/java/com/codeup/springblog/controllers/Post.java
@@ -1,10 +1,35 @@
 package com.codeup.springblog.controllers;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
 
-    private String title;
-    private String body;
+
+    @Id
+    @GeneratedValue
     private int id;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String body;
+
+    /**
+     *|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|
+     *|This mapping is equivalent to the following MySQL table definition:|
+     *|                                                                   |
+     *| CREATE TABLE ads (                                                |
+     *|    id BIGINT NOT NULL AUTO_INCREMENT,                             |
+     *|    title VARCHAR(100) NOT NULL,                                   |
+     *|    description VARCHAR(255) NOT NULL,                             |
+     *|    PRIMARY KEY (id)                                               |
+     *|);                                                                 |
+     *^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+     */
+
 
 
     public Post(String title,String body,int id) {
