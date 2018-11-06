331facbe06e5662a3547920df93cf4d3e2814f48
MichaelWaymire <mike.waymire2@gmail.com>
2018-11-05 22:16:20 -0600

    created UserDetailsLoader

--- src/main/java/com/codeup/springblog/services/UserRepo.java
+++ src/main/java/com/codeup/springblog/services/UserRepo.java
@@ -4,5 +4,5 @@ import com.codeup.springblog.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepo extends CrudRepository<User, Long> {
-    User findByUsername(String username)
+    User findByUsername(String username);
 }
